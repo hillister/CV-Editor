2a8eea242f5b0e306eb59b30b39f3d9e0dff2f8f
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-05-29 11:47:24 +0100

    hide display until submit button is clicked

--- src/GeneralInfo.jsx
+++ src/GeneralInfo.jsx
@@ -2,16 +2,55 @@ import { useState } from 'react'
 
 function GeneralInfo(){
     const [name, setName] = useState();
+    const [phone, setPhone] = useState();
+    const [email, setEmail] = useState();
+    const [address, setAddress] = useState();
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     function handleSetName(e){
         setName(e.target.value)
     }
+    
+    function handleSetPhone(e){
+        setPhone(e.target.value)
+    }
+
+    function handleSetEmail(e){
+        setEmail(e.target.value)
+    }
+
+    function handleSetAddress(e){
+        setAddress(e.target.value)
+    }
+
+    function handleClick(e){
+        e.preventDefault();
+        setIsSubmitted(true);
+    }
 
     return(
-        <>
-            <p>Name: {name}</p>
-            <input type='text' value={name} onChange={handleSetName} />
-        </>
+        <div className='generalInfo'>
+
+            <div className='infoDisplay' style={{display: isSubmitted ? 'block' : 'none'}}>
+                <h1>{name}</h1>
+                <h3>{phone}</h3>
+                <h3>{email}</h3>
+                <h3>{address}</h3>
+            </div>
+
+            
+            
+            <form>
+                <h4>General Info</h4>
+                <input placeholder='Name' type='text' value={name} onChange={handleSetName} /> <br/>
+                <input placeholder='Phone' type='number' value={phone} onChange={handleSetPhone} /> <br/>
+                <input placeholder='Email' type='text' value={email} onChange={handleSetEmail}/> <br/>
+                <input placeholder='Address' type='text' value={address} onChange={handleSetAddress} /> <br/>
+
+                <button onClick={handleClick}>Submit</button>
+            </form>
+
+        </div>
     )
 }
 
