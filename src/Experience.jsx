e06959eabaf44ff54ee9a3b2593901901c79f5b9
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-05-30 10:27:42 +0100

    add date input boxes

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -8,6 +8,9 @@ function Experience(){
     const [description, setDescription] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
 
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
     function handleCompanyName(e){
         setCompanyName(e.target.value)
     }
@@ -16,12 +19,16 @@ function Experience(){
         setPosition(e.target.value)
     }
 
-    function handleYear(e){
-        setYear(e.target.value)
-    }
-
     function handleDescription(e){
         setDescription(e.target.value)
+    }
+
+    function handleStartDate(e){
+        setStartDate(e.target.value)
+    }
+
+    function handleEndDate(e){
+        setEndDate(e.target.value)
     }
 
     function handleSubmit(){
@@ -37,14 +44,17 @@ function Experience(){
             <div style={{display: isSubmitted ? 'block' : 'none'}}>
                 <h3>Company: {companyName}</h3>
                 <h4>Position: {position}</h4>
-                <h4>Year: {year}</h4>
+                <h4>Year: {startDate} to {endDate}</h4>
                 <p>Description: {description}</p>
             </div>
 
             <form style={{display: isSubmitted ? 'none' : 'block'}}>
                 <input placeholder='Company Name' type="text" value={companyName} onChange={handleCompanyName}/><br/>
                 <input placeholder='Position' type="text" value={position} onChange={handlePosition}/><br/>
-                <input placeholder='Year' type="number" value={year} onChange={handleYear}/><br/>
+                <label>From: </label>
+                <input type="date" value={startDate} onChange={handleStartDate}/>
+                <label>To: </label>
+                <input type="date" value={endDate} onChange={handleEndDate}/><br/>
                 <textarea placeholder="Description" value={description} onChange={handleDescription}></textarea><br/>
             </form>
             <button onClick={handleSubmit}>{isSubmitted ? 'Edit' : 'Submit'}</button>
